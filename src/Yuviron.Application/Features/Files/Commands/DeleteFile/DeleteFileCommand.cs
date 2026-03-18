95eba5ef18658f4b434cdd07ec3e9a8469ae9b46
Dn. Shvts <thedanny2005@gmail.com>
2026-03-18 22:57:08 +0200

    refactor code and updaye logic for uploadFiles

--- src/Yuviron.Application/Features/Files/Commands/DeleteFile/DeleteFileCommand.cs
+++
@@ -1,5 +0,0 @@
-using MediatR;
-
-namespace Yuviron.Application.Features.Files.Commands.DeleteFile;
-
-public sealed record DeleteFileCommand(string FilePath) : IRequest<Unit>;
--- src/Yuviron.Application/Features/Users/Queries/GetUserProfile/GetUserProfileQuery.cs
+++
@@ -1,5 +0,0 @@
-﻿using MediatR;
-
-namespace Yuviron.Application.Features.Users.Queries.GetUserProfile;
-
-public record GetUserProfileQuery(Guid UserId) : IRequest<UserProfileDTO>;
